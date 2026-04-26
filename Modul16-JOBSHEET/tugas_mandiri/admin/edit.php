4660f806836bd0aeb4f0ee431f938c2b42a8e11a
Latarombo <zidane544@gmail.com>
2026-04-26 20:13:37 +0700

    Upload

--- Modul16-JOBSHEET/tugas_mandiri/admin/edit.php
+++ Modul16-JOBSHEET/tugas_mandiri/admin/edit.php
@@ -21,22 +21,122 @@ if (!$d) {
     echo "Data tidak ditemukan!";
     exit;
 }
+
+$hobi_list   = explode(",", $d['hobi']);
+$ekskul_list = explode(",", $d['ekskul']);
+
+$ttl_parts = explode("-", $d['ttl']);
+$thn_val = $ttl_parts[0] ?? '';
+$bln_val = $ttl_parts[1] ?? '';
+$tgl_val = $ttl_parts[2] ?? '';
 ?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>Edit Data Siswa</title>
+</head>
+<body>
+
+<h2>Edit Data Siswa</h2>
 
 <form action="proses_update.php" method="POST">
   <input type="hidden" name="nis" value="<?= htmlspecialchars($d['nis']); ?>">
 
-  Nama: <input type="text" name="nama" value="<?= htmlspecialchars($d['nama']); ?>"><br><br>
+  <table border="0" cellpadding="5">
 
-  Kelas:
-  <select name="kelas">
-    <option value="X" <?= $d['kelas'] == 'X' ? 'selected' : '' ?>>X</option>
-    <option value="XI" <?= $d['kelas'] == 'XI' ? 'selected' : '' ?>>XI</option>
-    <option value="XII" <?= $d['kelas'] == 'XII' ? 'selected' : '' ?>>XII</option>
-  </select><br><br>
+    <tr>
+      <td>NIS</td>
+      <td>:</td>
+      <td><?= htmlspecialchars($d['nis']); ?> <i>(tidak dapat diubah)</i></td>
+    </tr>
 
-  Ekskul:
-  <input type="text" name="ekskul" value="<?= htmlspecialchars($d['ekskul']); ?>"><br><br>
+    <tr>
+      <td>Nama</td>
+      <td>:</td>
+      <td><input type="text" name="nama" value="<?= htmlspecialchars($d['nama']); ?>" required></td>
+    </tr>
 
-  <button type="submit">Update</button>
+    <tr>
+      <td>Kelas</td>
+      <td>:</td>
+      <td>
+        <select name="kelas" required>
+          <option value="X"   <?= $d['kelas'] == 'X'   ? 'selected' : '' ?>>X</option>
+          <option value="XI"  <?= $d['kelas'] == 'XI'  ? 'selected' : '' ?>>XI</option>
+          <option value="XII" <?= $d['kelas'] == 'XII' ? 'selected' : '' ?>>XII</option>
+        </select>
+      </td>
+    </tr>
+
+    <tr>
+      <td>Tgl Lahir</td>
+      <td>:</td>
+      <td>
+        <input type="text" name="tgl" size="2" placeholder="DD"   value="<?= htmlspecialchars($tgl_val); ?>" required> /
+        <input type="text" name="bln" size="2" placeholder="MM"   value="<?= htmlspecialchars($bln_val); ?>" required> /
+        <input type="text" name="thn" size="4" placeholder="YYYY" value="<?= htmlspecialchars($thn_val); ?>" required>
+      </td>
+    </tr>
+
+    <tr>
+      <td>Alamat</td>
+      <td>:</td>
+      <td><textarea name="alamat" cols="30" rows="3" required><?= htmlspecialchars($d['alamat']); ?></textarea></td>
+    </tr>
+
+    <tr>
+      <td>Kota</td>
+      <td>:</td>
+      <td><input type="text" name="kota" value="<?= htmlspecialchars($d['kota']); ?>" required></td>
+    </tr>
+
+    <tr>
+      <td>Jenis Kelamin</td>
+      <td>:</td>
+      <td>
+        <input type="radio" name="jk" value="L" <?= $d['jk'] == 'L' ? 'checked' : '' ?> required> Laki-Laki
+        <input type="radio" name="jk" value="P" <?= $d['jk'] == 'P' ? 'checked' : '' ?>> Perempuan
+      </td>
+    </tr>
+
+    <tr>
+      <td>Hobi</td>
+      <td>:</td>
+      <td>
+        <input type="checkbox" name="hobi[]" value="Membaca"   <?= in_array("Membaca",   $hobi_list) ? 'checked' : '' ?>> Membaca<br>
+        <input type="checkbox" name="hobi[]" value="Olahraga"  <?= in_array("Olahraga",  $hobi_list) ? 'checked' : '' ?>> Olahraga<br>
+        <input type="checkbox" name="hobi[]" value="Menyanyi"  <?= in_array("Menyanyi",  $hobi_list) ? 'checked' : '' ?>> Menyanyi<br>
+        <input type="checkbox" name="hobi[]" value="Menari"    <?= in_array("Menari",    $hobi_list) ? 'checked' : '' ?>> Menari<br>
+        <input type="checkbox" name="hobi[]" value="Traveling" <?= in_array("Traveling", $hobi_list) ? 'checked' : '' ?>> Traveling
+      </td>
+    </tr>
+
+    <tr>
+      <td>Ekskul</td>
+      <td>:</td>
+      <td>
+        <select name="ekskul[]" multiple size="7">
+          <option value="Pramuka"      <?= in_array("Pramuka",      $ekskul_list) ? 'selected' : '' ?>>Pramuka</option>
+          <option value="Basket"       <?= in_array("Basket",       $ekskul_list) ? 'selected' : '' ?>>Basket</option>
+          <option value="Volly"        <?= in_array("Volly",        $ekskul_list) ? 'selected' : '' ?>>Volly</option>
+          <option value="Band"         <?= in_array("Band",         $ekskul_list) ? 'selected' : '' ?>>Band</option>
+          <option value="Seni Tari"    <?= in_array("Seni Tari",    $ekskul_list) ? 'selected' : '' ?>>Seni Tari</option>
+          <option value="Robotic"      <?= in_array("Robotic",      $ekskul_list) ? 'selected' : '' ?>>Robotic</option>
+          <option value="Bulu Tangkis" <?= in_array("Bulu Tangkis", $ekskul_list) ? 'selected' : '' ?>>Bulu Tangkis</option>
+        </select>
+      </td>
+    </tr>
+
+    <tr>
+      <td colspan="3">
+        <button type="submit">Update</button>
+        <a href="admin.php">Batal</a>
+      </td>
+    </tr>
+
+  </table>
 </form>
+
+</body>
+</html>
--- Modul16-JOBSHEET/tugas_mandiri/admin/proses_update.php
+++ Modul16-JOBSHEET/tugas_mandiri/admin/proses_update.php
@@ -6,10 +6,24 @@ include '../login_level/koneksi.php';
 $nis    = $_POST['nis'];
 $nama   = $_POST['nama'];
 $kelas  = $_POST['kelas'];
-$ekskul = $_POST['ekskul'];
+$alamat = $_POST['alamat'];
+$kota   = $_POST['kota'];
+$jk     = $_POST['jk'];
 
-$stmt = $conn->prepare("UPDATE tb_siswa SET nama=?, kelas=?, ekskul=? WHERE nis=?");
-$stmt->bind_param("ssss", $nama, $kelas, $ekskul, $nis);
+// Gabung tanggal
+$tgl = $_POST['tgl'];
+$bln = $_POST['bln'];
+$thn = $_POST['thn'];
+$ttl = $thn . "-" . $bln . "-" . $tgl;
+
+// Hobi
+$hobi = isset($_POST['hobi']) ? implode(",", $_POST['hobi']) : "";
+
+// Ekskul multiple
+$ekskul = isset($_POST['ekskul']) ? implode(",", $_POST['ekskul']) : "";
+
+$stmt = $conn->prepare("UPDATE tb_siswa SET nama=?, kelas=?, ttl=?, alamat=?, kota=?, jk=?, hobi=?, ekskul=? WHERE nis=?");
+$stmt->bind_param("sssssssss", $nama, $kelas, $ttl, $alamat, $kota, $jk, $hobi, $ekskul, $nis);
 $stmt->execute();
 $stmt->close();
 
